b51ae1051dc0dfc1439e5750aadba68eb2ddcc44
Huizhen Wu <huizhen.wu@Huizhens-Mac-mini.local>
2016-05-26 21:14:37 -0400

    Add NavigationBar Project

---
+++ NavigationBar/NavigationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by Huizhen Wu on 2016-05-26.
+//  Copyright © 2016 Huizhen Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
